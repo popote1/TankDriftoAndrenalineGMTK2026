b1fb86c16f5e376964cb551cde01bc710d1fb5a6
popote <yvan.bladet@gmail.com>
2026-07-23 22:52:58 +0200

    DriftFactor+ Visual Tilte

--- Assets/Scripte/TankController.cs
+++ Assets/Scripte/TankController.cs
@@ -1,5 +1,4 @@
 using System;
-using TMPro;
 using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -28,27 +27,39 @@ public class TankController : MonoBehaviour
     [SerializeField] private float TankAccelleration = 1;
     [SerializeField] private AnimationCurve _accelerationCurve;
     [Header("Steering")] [SerializeField] private float _wheelMaxAngle = 45;
-    [SerializeField] private float _wheelBrakePower = 1;
+    
     [SerializeField] private float _upWardForce = 1;
-    [SerializeField] private float _upwardDamper = 1;
+    [SerializeField] private float _driftSpeedPresservation = 1;
+    [SerializeField] private AnimationCurve _driftSpeedPreservationCurve;
+    [Header("DriftFactor")]
+    
+    [SerializeField] private AnimationCurve _driftFactorSpeedCurve;
+    [SerializeField] private AnimationCurve _driftFactorDotCurve;
+    
     [SerializeField] private CinemachineCamera _virtualCamera;
     [SerializeField] private float _baseFow = 60f;
     [SerializeField] private float _speedFow = 70f;
     [SerializeField] private LayerMask _groundMask;
     //[SerializeField]private float _moveSpeed;
-    [SerializeField]private float _rotationSpeed;
+    //[SerializeField]private float _rotationSpeed;
     
     private InputAction _moveAction;
     private int wheelGrounded;
     [SerializeField] private bool _controlBlock;
-    void Start()
-    {
+    
+    public float GetCurrentSpeed{get => Vector3.Dot(transform.forward, _rb.linearVelocity);}
+    public float GetNormalizedSpeed{get => Mathf.Clamp01(Mathf.Abs(GetCurrentSpeed)/_tankTopSpeed);}
+
+    public float GetGurentFrontGrip { get => _frontwheelGripFactor.Evaluate(GetNormalizedSpeed); }
+    public float GetGurentBackGrip { get => _backwheelGripFactor.Evaluate(GetNormalizedSpeed); }
+    public float DriftFactor;
+    public float TurnDot;
+
+    void Start() {
         _rb = GetComponent<Rigidbody>();
         _moveAction = InputSystem.actions.FindAction("Move");
         StaticEvent.OnGameStart += InstanceOnOnGameStart;
         StaticEvent.OnBlockPlayerControl+= StaticEventOnOnBlockPlayerControl;
-        
-        
     }
 
     private void OnDestroy() {
@@ -68,12 +79,29 @@ public class TankController : MonoBehaviour
         
         //ManagerMovement();
         if( !_controlBlock) ManagerRotation();
+        CalculateDriftFactor();
         ManageCameraFow();
+        
     }
 
     private void FixedUpdate() {
         ManageHover();
         ManagerStayUpWard();
+    }
+
+    private void CalculateDriftFactor()
+    {
+        float dot = _driftFactorDotCurve.Evaluate(Vector3.Dot(transform.forward, _rb.linearVelocity.normalized));
+        float speed = _driftFactorSpeedCurve.Evaluate(_rb.linearVelocity.magnitude / _tankTopSpeed);
+        DriftFactor = dot * speed;
+
+        if (_rb.linearVelocity.magnitude > 1)
+        {
+            TurnDot = Vector3.Dot(transform.right, _rb.linearVelocity.normalized);
+        }
+        else {
+            TurnDot = 0;
+        }
     }
 
     private void ManageHover() {
@@ -122,8 +150,10 @@ public class TankController : MonoBehaviour
             float desiredVelChange = -steeringVel * gripFactor;
             float desiredAccel = desiredVelChange*Time.fixedDeltaTime;
             _rb.AddForceAtPosition(steeringDir*_wheelMass * desiredAccel, wheel.position);
-            
-            
+
+            float driftPreservationFactor =
+                _driftSpeedPreservationCurve.Evaluate(normalizedSpeed) * _driftSpeedPresservation;
+            _rb.AddForceAtPosition(accelDir * driftPreservationFactor * Mathf.Abs(desiredAccel), wheel.position);
                 
             
             //Accelration
---
+++ Assets/Scripte/VisualTilte.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class VisualTilte : MonoBehaviour
+{
+    [SerializeField] private TankController _tankController;
+    [SerializeField]private float _maxTiltAngle = 10;
+
+    private void Update()
+    {
+        float t = _tankController.TurnDot / 2 + 0.5f;
+        Debug.unityLogger.Log("time", t);
+        float tilte = Mathf.Lerp( _maxTiltAngle, -_maxTiltAngle,t);
+        transform.localEulerAngles = new Vector3(0, 0, tilte);
+    }
+}
