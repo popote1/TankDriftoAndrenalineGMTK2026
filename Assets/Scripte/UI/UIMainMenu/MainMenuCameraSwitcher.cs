4bda2f595f5bc3cd45f9c1962874618a611a5a9d
popote <yvan.bladet@gmail.com>
2026-07-26 12:30:14 +0200

    MainMenu Fix

--- Assets/Scripte/UI/UIMainMenu/MainMenuCameraSwitcher.cs
+++ Assets/Scripte/UI/UIMainMenu/MainMenuCameraSwitcher.cs
@@ -1,45 +1,55 @@
 ﻿using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class MainMenuCameraSwitcher : MonoBehaviour
 {
-    [SerializeField] private GameObject _cameraMainMenu;
-    [SerializeField] private GameObject _cameraLevelSelection;
-    [SerializeField] private GameObject _cameraOption;
+    [SerializeField] private CinemachineCamera _cameraMainMenu;
+    [SerializeField] private CinemachineCamera _cameraLevelSelection;
+    [SerializeField] private CinemachineCamera _cameraOption;
+    [SerializeField] private CinemachineCamera _cameraCredit;
     
     [SerializeField] private UIPanel _mainMenuPanel;
     [SerializeField] private UIPanel _mainStageSelectionPanel;
     [SerializeField] private UIPanel _optionsPanel;
+    [SerializeField] private UIPanel _creditPanel;
 
     private void Start()
     {
         _mainMenuPanel.OnPanelOpen+= OnMainMenuOpen;
         _mainStageSelectionPanel.OnPanelOpen += OnStageSelectionOpen;
         _optionsPanel.OnPanelOpen += OnOptionsOpen;
+        _creditPanel.OnPanelOpen += OnCreditOpen;
     }
 
     
 
     private void TurnOffAllCams() {
-        _cameraMainMenu.SetActive(false);
-        _cameraLevelSelection.SetActive(false);
-        _cameraOption.SetActive(false);
+        _cameraMainMenu.Priority.Value=0;
+        _cameraLevelSelection.Priority.Value=0;
+        _cameraOption.Priority.Value=0;
+        _cameraCredit.Priority.Value=0;
     }
 
     private void OnMainMenuOpen(object sender, EventArgs e)
     {
         TurnOffAllCams();
-        _cameraMainMenu.SetActive(true);
+        _cameraMainMenu.Priority.Value=1;
     }
     private void OnStageSelectionOpen(object sender, EventArgs e)
     {
         TurnOffAllCams();
-        _cameraLevelSelection.SetActive(true);
+        _cameraLevelSelection.Priority.Value=1;
     }
 
     private void OnOptionsOpen(object sender, EventArgs e)
     {
         TurnOffAllCams();
-        _cameraOption.SetActive(true);
+        _cameraOption.Priority.Value=1;
+    }
+    public void OnCreditOpen(object sender, EventArgs e)
+    {
+        TurnOffAllCams();
+        _cameraCredit.Priority.Value=1;
     }
 }
--- Assets/Scripte/UI/UIMainMenu/UIPanel.cs
+++ Assets/Scripte/UI/UIMainMenu/UIPanel.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class UIPanel : MonoBehaviour
 {
     public GameObject UIVisualPanel;
+    public UIPanelMaineMenuFeedBack UIPanelFeedBack;
     public event EventHandler OnPanelClose;
     public event EventHandler OnPanelOpen;
 
     public virtual void OpenPanel() {
         UIVisualPanel.SetActive(true);
+        if(UIPanelFeedBack != null)UIPanelFeedBack.OpenPanel();
         OnPanelOpen?.Invoke(this, EventArgs.Empty);
     }
 
     protected virtual void ClosePanel() {
-        UIVisualPanel.SetActive(false);
+        if(UIPanelFeedBack != null)UIPanelFeedBack.ClosePanel();
+        else UIVisualPanel.SetActive(false);
         OnPanelClose?.Invoke(this, EventArgs.Empty);
     }
 }
